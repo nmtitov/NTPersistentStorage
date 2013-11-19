ff6390a617478ed7358e1e67860c91433e9a3142
Nikita <nmtitov@gmail.com>
2013-11-19 19:34:06 +0400

    Initial Commit

---
+++ NTPersistentStorage/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  NTPersistentStorage
+//
+//  Created by Никита on 19.11.13.
+//  Copyright (c) 2013 pawzzle.net. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
